20c83dede384331be367aba7bd182e4a7c1e6197
eduardoworrel <=>
2022-01-17 21:34:10 -0300

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
+import {Route, Routes} from 'react-router-dom'
 import './App.css';
+import Count from './pages/Count';
+import Home from './pages/Home';
+import {ContadorProvider} from './contexts/Contador';
+import Menu from './components/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <ContadorProvider>
+    <Menu />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/count" element={<Count />}/>
+    </Routes>
+  </ContadorProvider>
+  </>
   );
 }
 
